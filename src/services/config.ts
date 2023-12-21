abd0fedaed69f4570c88340f8d658ee196c1a031
BangDori <qudwns8865@naver.com>
2023-12-21 19:39:34 +0900

    fix: api 통신 기본 요청에 accessToken 제거

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import Cookies from 'js-cookie';
 
 import { SERVER_URL } from '@constants/api';
 import { CustomError } from '@custom/types/response';
@@ -8,7 +7,6 @@ const axiosInstance = axios.create({
   baseURL: SERVER_URL,
   timeout: 20000,
   headers: {
-    'Authorization-Access': Cookies.get('accesstoken'),
     'Content-Type': 'application/json',
     accept: 'application/json',
   },
