434aada2711e7f28d046701ac7af77322d0c78a3
BangDori <qudwns8865@naver.com>
2024-01-10 03:36:04 +0900

    feat: API 응답 타입 설정 및 CRUD 구조 설계

---
+++ src/services/crud.ts
@@ -0,0 +1,124 @@
+import { AxiosRequestConfig } from 'axios';
+
+import axiosInstance from './config';
+
+/**
+ * API 요청 성공 타입
+ * isSuccess: true
+ * data: T
+ */
+export interface SuccessResponse<T> {
+  isSuccess: boolean;
+  data: T;
+}
+
+/**
+ * API 요청 실패 타입
+ * isSuccess: false
+ * errorCode: 커스텀 에러 코드
+ * errorMessage: 커스텀 에러 메시지
+ * status: 상태 코드
+ */
+export interface FailureResponse {
+  isSuccess: boolean;
+  errorCode: string;
+  errorMessage: string;
+}
+
+type Response<T> = SuccessResponse<T> | FailureResponse;
+
+const client = {
+  /**
+   * GET 요청
+   * @param url Request URI
+   * @param config Request Config (Headers, ...)
+   * @returns SuccessResponse | FailureResponse
+   */
+  get: async <T>(
+    url: string,
+    config?: AxiosRequestConfig,
+  ): Promise<Response<T>> => {
+    try {
+      const response = await axiosInstance.get<T>(url, config);
+
+      return {
+        isSuccess: true,
+        data: response.data,
+      };
+    } catch (error: any) {
+      return error;
+    }
+  },
+
+  /**
+   * POST 요청
+   * @param url Request URI
+   * @param data Request Data
+   * @param config Request Config (Headers, ...)
+   * @returns SuccessResponse | FailureResponse
+   */
+  post: async <T>(
+    url: string,
+    data?: any,
+    config?: AxiosRequestConfig,
+  ): Promise<Response<T>> => {
+    try {
+      const response = await axiosInstance.post<T>(url, data, config);
+
+      return {
+        isSuccess: true,
+        data: response.data,
+      };
+    } catch (error: any) {
+      return error;
+    }
+  },
+
+  /**
+   * PUT 요청
+   * @param url Request URI
+   * @param data Request Data
+   * @param config Request Config (Headers, ...)
+   * @returns SuccessResponse | FailureResponse
+   */
+  put: async <T>(
+    url: string,
+    data?: any,
+    config?: AxiosRequestConfig,
+  ): Promise<Response<T>> => {
+    try {
+      const response = await axiosInstance.put<T>(url, data, config);
+
+      return {
+        isSuccess: true,
+        data: response.data,
+      };
+    } catch (error: any) {
+      return error;
+    }
+  },
+
+  /**
+   * DELETE 요청
+   * @param url REQUEST URI
+   * @param config Request Config (Headers, ...)
+   * @returns SuccessResponse | FailureResponse
+   */
+  delete: async <T>(
+    url: string,
+    config?: AxiosRequestConfig,
+  ): Promise<Response<T>> => {
+    try {
+      const response = await axiosInstance.delete<T>(url, config);
+
+      return {
+        isSuccess: true,
+        data: response.data,
+      };
+    } catch (error: any) {
+      return error;
+    }
+  },
+};
+
+export default client;
