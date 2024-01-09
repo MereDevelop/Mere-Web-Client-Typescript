1d469519d0fb918d23bbc75b7c03c317f8aa966f
whitecity01 <kh081104@gmail.com>
2024-01-09 17:39:44 +0900

    [Feat] signup 임시 route 추가

--- src/routes/auth/signup.tsx
+++ src/routes/auth/signup.tsx
@@ -1,9 +1,27 @@
 import { RouteObject } from 'react-router-dom';
-import SignupPage from '@pages/auth/SignupPage';
+import SignupPage from '@pages/auth/signup/SignupPage';
+import OwnerInfoPage, {
+  ownerInfoSubmit,
+} from '@pages/auth/signup/OwnerInfoPage';
+import { loadCurrentStatus } from '@pages/auth/account/AccountPage';
 
 const signupRoute: RouteObject = {
-  index: true,
+  path: 'signup',
   element: <SignupPage />,
+  loader: loadCurrentStatus,
+  action: ownerInfoSubmit,
+  children: [
+    {
+      index: true,
+      element: <OwnerInfoPage />,
+    },
+    {
+      path: 'ownerinfo',
+    },
+    {
+      path: 'wait',
+    },
+  ],
 };
 
 export default signupRoute;
