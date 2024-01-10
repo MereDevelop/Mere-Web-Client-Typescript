9d09a021e7e60a18e9e333ab549a87444c2a3bc4
whitecity01 <kh081104@gmail.com>
2024-01-11 04:51:54 +0900

    feat: OwnerInfoProps type 추가

--- src/custom/types/signup/Signup.ts
+++ src/custom/types/signup/Signup.ts
@@ -3,3 +3,7 @@ export type SignupStatusType = 'signup' | 'ownerInfo' | 'wait';
 export type StoreInfoProps = {
   isSubmitting: boolean;
 };
+
+export type OwnerInfoProps = {
+  isSubmitting: boolean;
+};
