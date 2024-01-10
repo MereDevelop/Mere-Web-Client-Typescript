e1e9c7bb5bc42244e13226b09b75f8768888fd6b
BangDori <qudwns8865@naver.com>
2024-01-11 01:37:13 +0900

    refactor: instance 위치 이동 및 파일명 수정 - config -> interceptor - crud -> config
    axios instance 위치 config로 이동

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,14 +1,12 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 
 import { SERVER_URL } from '@constants/api';
-import { FailureResponse } from '@services/crud';
 import {
-  TokenExpirationErrorCodeType,
-  UserTokenErrorCodeType,
-} from '@custom/types/error/errorCode';
-import { getRefreshToken } from '@utils/token';
+  FailureResponse,
+  SuccessResponse,
+} from '@custom/types/common/Response';
 
-const axiosInstance = axios.create({
+export const axiosInstance = axios.create({
   baseURL: SERVER_URL,
   timeout: 20000,
   headers: {
@@ -17,82 +15,100 @@ const axiosInstance = axios.create({
   },
 });
 
-axiosInstance.interceptors.response.use(
-  async (response) => {
-    const { data, request } = response;
-    const originalRequest = request.responseURL;
+type Response<T> = SuccessResponse<T> | FailureResponse;
 
-    // 토큰 체크 후 재발급하는 로직 추가
-    if (isUserTokenErrorCode(data.errorCode)) {
-      window.location.href = '/';
-    }
+const client = {
+  /**
+   * GET 요청
+   * @param url Request URI
+   * @param config Request Config (Headers, ...)
+   * @returns SuccessResponse<T> | FailureResponse
+   */
+  get: async <T>(
+    url: string,
+    config?: AxiosRequestConfig,
+  ): Promise<Response<T>> => {
+    try {
+      const response = await axiosInstance.get<T>(url, config);
 
-    if (isTokenExpirationErrorCode(data.errorCode)) {
-      const tokenResponse = await reIssueAccessTokenForOwner();
-
-      if (tokenResponse.data) {
-        const { accessToken } = tokenResponse.data;
-        axiosInstance.defaults.headers.common['Authorization-Access'] =
-          accessToken;
-
-        const originalResponse = await axiosInstance(originalRequest);
-        return originalResponse;
-      }
-    }
-
-    return response;
-  },
-  async (error) => {
-    if (error.errorCode === 'XX0000') {
+      return {
+        isSuccess: true,
+        data: response.data,
+      };
+    } catch (error: any) {
       return error;
     }
-
-    const { data } = error.response;
-    const { errorCode, errorMessage } = data;
-
-    const customError: FailureResponse = {
-      isSuccess: false,
-      errorCode,
-      errorMessage,
-    };
-
-    return Promise.reject(customError);
   },
-);
 
-function isUserTokenErrorCode(
-  errorCode: string,
-): errorCode is UserTokenErrorCodeType {
-  if (errorCode === 'FT0000') return true;
-  if (errorCode === 'FT0002') return true;
-  if (errorCode === 'FT0005') return true;
+  /**
+   * POST 요청
+   * @param url Request URI
+   * @param data Request Data
+   * @param config Request Config (Headers, ...)
+   * @returns SuccessResponse<T> | FailureResponse
+   */
+  post: async <T>(
+    url: string,
+    data?: any,
+    config?: AxiosRequestConfig,
+  ): Promise<Response<T>> => {
+    try {
+      const response = await axiosInstance.post<T>(url, data, config);
 
-  return false;
-}
+      return {
+        isSuccess: true,
+        data: response.data,
+      };
+    } catch (error: any) {
+      return error;
+    }
+  },
 
-function isTokenExpirationErrorCode(
-  errorCode: string,
-): errorCode is TokenExpirationErrorCodeType {
-  if (errorCode === 'FT0001') return true;
-  if (errorCode === 'FT0003') return true;
-  if (errorCode === 'FT0004') return true;
+  /**
+   * PUT 요청
+   * @param url Request URI
+   * @param data Request Data
+   * @param config Request Config (Headers, ...)
+   * @returns SuccessResponse<T> | FailureResponse
+   */
+  put: async <T>(
+    url: string,
+    data?: any,
+    config?: AxiosRequestConfig,
+  ): Promise<Response<T>> => {
+    try {
+      const response = await axiosInstance.put<T>(url, data, config);
 
-  return false;
-}
+      return {
+        isSuccess: true,
+        data: response.data,
+      };
+    } catch (error: any) {
+      return error;
+    }
+  },
 
-export async function reIssueAccessTokenForOwner() {
-  const refreshToken = getRefreshToken();
-  if (!refreshToken) return null;
+  /**
+   * DELETE 요청
+   * @param url REQUEST URI
+   * @param config Request Config (Headers, ...)
+   * @returns SuccessResponse<T> | FailureResponse
+   */
+  delete: async <T>(
+    url: string,
+    config?: AxiosRequestConfig,
+  ): Promise<Response<T>> => {
+    try {
+      const response = await axiosInstance.delete<T>(url, config);
 
-  const response = await axiosInstance({
-    method: 'get',
-    url: '/owner/update/access-token',
-    headers: {
-      'Authorization-Refresh': refreshToken,
-    },
-  });
+      return {
+        isSuccess: true,
+        data: response.data,
+      };
+    } catch (error: any) {
+      return error;
+    }
+  },
+};
 
-  return response;
-}
-
-export default axiosInstance;
+export default client;
