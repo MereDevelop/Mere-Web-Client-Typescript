abedb0dd9f65b6b73dafc9d44e55013bb0babeac
whitecity01 <kh081104@gmail.com>
2024-01-11 02:57:53 +0900

    feat: StoreInfo 로직 구현

--- src/components/auth/signup/StoreInfo.tsx
+++ src/components/auth/signup/StoreInfo.tsx
@@ -13,13 +13,26 @@ const StoreInfo: React.FC<StoreInfoProps> = ({ isSubmitting }) => {
     string | undefined
   >();
 
+  const isAllFilled = (e: { preventDefault: () => void }) => {
+    if (!storeName || !storePhone || !storeAddress || !storeAddressDetail) {
+      e.preventDefault();
+      alert('회원가입 양식을 모두 입력해주세요.');
+    }
+  };
+
   return (
-    <Form className='store-info-form' method='post' action='/signup'>
+    <Form
+      className='store-info-form'
+      method='post'
+      onSubmit={isAllFilled}
+      action='/signup'
+    >
       <div className='store-info-input-container'>
         <div className='store-name-container'>
           <label htmlFor='storeName'>매장명(상호명)</label>
           <input
             id='storeName'
+            name='storeName'
             type='text'
             placeholder='매장명'
             onChange={(e) => {
@@ -31,6 +44,7 @@ const StoreInfo: React.FC<StoreInfoProps> = ({ isSubmitting }) => {
           <label htmlFor='storePhone'>사장님 전화번호</label>
           <input
             id='storePhone'
+            name='storePhone'
             type='number'
             placeholder='‘-’빼고 숫자만 입력'
             onChange={(e) => {
@@ -41,15 +55,27 @@ const StoreInfo: React.FC<StoreInfoProps> = ({ isSubmitting }) => {
         <div className='store-address-container'>
           <label htmlFor='store-address'>매장주소</label>
           <div className='store-address-search-container'>
-            <input id='storeAddress' type='text' placeholder='매장주소 입력' />
+            <input
+              id='storeAddress'
+              name='storeAddress'
+              type='text'
+              placeholder='매장주소 입력'
+              onChange={(e) => {
+                setStoreAddress(e.target.value);
+              }}
+            />
             <button type='button' className='store-address-search-btn'>
               검색
             </button>
           </div>
           <input
             id='storeAddressDetail'
+            name='storeAddressDetail'
             type='text'
             placeholder='상세주소 입력'
+            onChange={(e) => {
+              setStoreAddressDetail(e.target.value);
+            }}
           />
         </div>
       </div>
