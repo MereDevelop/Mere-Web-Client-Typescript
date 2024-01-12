0a9e59fbae2fb0b6c08ce6760fd4e30c5e4cd6ba
BangDori <qudwns8865@naver.com>
2024-01-12 10:20:22 +0900

    fix: 토큰 재발급 함수 의존성 사이클 해결 - config의 client 활용 -> axios 대체

--- src/services/auth/token.ts
+++ src/services/auth/token.ts
@@ -1,18 +1,30 @@
+import axios from 'axios';
+
+import { SERVER_URL } from '@constants/api';
 import { AccessTokenDTO } from '@custom/types/common/Token';
-import client from '@services/client';
+import {
+  FailureResponse,
+  SuccessResponse,
+} from '@custom/types/common/Response';
 import { getRefreshToken } from '@utils/token';
 
 export async function reIssueAccessToken() {
   const refreshToken = getRefreshToken();
   if (!refreshToken) window.location.href = '/';
 
-  const response = await client
-    .get<AccessTokenDTO>('/owner/update/access-token', {
+  const response = await axios
+    .get<AccessTokenDTO>(`${SERVER_URL}/owner/update/access-token`, {
       headers: {
         'Authorization-Refresh': refreshToken,
       },
     })
-    .then((resData) => resData);
+    .then(
+      (resData): SuccessResponse<AccessTokenDTO> => ({
+        isSuccess: true,
+        data: resData.data,
+      }),
+    )
+    .catch((error): FailureResponse => error);
 
   return response;
 }
