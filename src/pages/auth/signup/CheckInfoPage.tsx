1eae1a59018deaa98ac31f2636b760b3ff9d7ed5
whitecity01 <kh081104@gmail.com>
2024-01-13 21:38:58 +0900

    feat: CheckInfoPage 회원가입 요청 action 구현

--- src/pages/auth/signup/CheckInfoPage.tsx
+++ src/pages/auth/signup/CheckInfoPage.tsx
@@ -1,6 +1,7 @@
 import CheckInfoForm from '@components/auth/signup/CheckInfoForm';
+import { requestSignup } from '@services/auth/signup';
 import { SignupInfo, signupForm } from '@store/signup-dto';
-import { useLoaderData } from 'react-router-dom';
+import { redirect, useLoaderData } from 'react-router-dom';
 
 const CheckInfoPage = () => {
   const signupInfo = useLoaderData() as SignupInfo | null;
@@ -12,6 +13,17 @@ export default CheckInfoPage;
 export async function loadSignupForm() {
   const { getSignupInfo } = signupForm.getState();
   const signupInfo: SignupInfo = getSignupInfo(); // 작성한 회원가입 폼 가져오기
-  console.log(signupInfo);
   return signupInfo;
+}
+
+export async function signupRequest() {
+  const { getSignupInfo } = signupForm.getState();
+  const signupInfo: SignupInfo = getSignupInfo();
+  const res = await requestSignup(signupInfo);
+
+  if (res === 200) {
+    return redirect('/signup/wait');
+  }
+  alert('회원가입 중 오류가 발생하였습니다.');
+  return null;
 }
