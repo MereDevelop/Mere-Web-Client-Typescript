1bc69649409851b3747e75d42f54aaa5b644f013
whitecity01 <kh081104@gmail.com>
2024-01-15 04:25:48 +0900

    feat: 전화번호 인증 및 검증 api 추가

--- src/services/auth/signup.ts
+++ src/services/auth/signup.ts
@@ -1,5 +1,6 @@
 import { SERVER_URL } from '@constants/api';
 import { CustomError } from '@custom/types/response';
+import { verifyCodeDto, verifyDto } from '@custom/types/signup/Signup';
 import axios from '@services/config';
 import { SignupInfo } from '@store/signup-dto';
 
@@ -8,6 +9,36 @@ export async function requestSignup(signupForm: SignupInfo) {
     method: 'post',
     url: `${SERVER_URL}/owner/sign/up`,
     data: signupForm,
+  })
+    .then((resData) => {
+      const { status } = resData;
+      return status;
+    })
+    .catch((error): CustomError => error);
+
+  return response;
+}
+
+export async function requestVerifyNumber(verifyDto: verifyDto) {
+  const response = await axios({
+    method: 'post',
+    url: `${SERVER_URL}/owner/sign/in/phone-verification-request`,
+    data: verifyDto,
+  })
+    .then((resData) => {
+      const { status } = resData;
+      return status;
+    })
+    .catch((error): CustomError => error);
+
+  return response;
+}
+
+export async function verifyNumber(verifyCodeDto: verifyCodeDto) {
+  const response = await axios({
+    method: 'post',
+    url: `${SERVER_URL}/owner/sign/in/phone-verification`,
+    data: verifyCodeDto,
   })
     .then((resData) => {
       const { status } = resData;
