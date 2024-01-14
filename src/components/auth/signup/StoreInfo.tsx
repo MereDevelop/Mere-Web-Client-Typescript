98800179f1f481c55b3baadee0ae4c155ca1d97e
whitecity01 <kh081104@gmail.com>
2024-01-15 01:21:55 +0900

    feat: 매장주소 검색 카카오 api 연동

--- src/components/auth/signup/StoreInfo.tsx
+++ src/components/auth/signup/StoreInfo.tsx
@@ -1,9 +1,9 @@
 import { Form } from 'react-router-dom';
 import BeatLoader from '@commons/BeatLoader';
-import '@styles/auth/account/Verification.scss';
 import { StoreInfoProps } from '@custom/types/signup/Signup';
 import { useEffect, useState } from 'react';
 import '@styles/auth/signup/StoreInfoForm.scss';
+import StoreSearchModal from '@commons/modal/StoreSearchModal';
 
 const StoreInfo: React.FC<StoreInfoProps> = ({ isSubmitting }) => {
   const [storeName, setStoreName] = useState<string | undefined>();
@@ -13,6 +13,7 @@ const StoreInfo: React.FC<StoreInfoProps> = ({ isSubmitting }) => {
     string | undefined
   >();
   const [isValid, setIsVaild] = useState<boolean>(false);
+  const [isSearchToggle, setIsSearchToggle] = useState(false);
 
   useEffect(() => {
     const isAllFilled = () => {
@@ -58,11 +59,16 @@ const StoreInfo: React.FC<StoreInfoProps> = ({ isSubmitting }) => {
               name='storeAddress'
               type='text'
               placeholder='매장주소 입력'
+              value={storeAddress}
               onChange={(e) => {
                 setStoreAddress(e.target.value);
               }}
             />
-            <button type='button' className='store-address-search-btn'>
+            <button
+              type='button'
+              className='store-address-search-btn'
+              onClick={() => setIsSearchToggle(!isSearchToggle)}
+            >
               검색
             </button>
           </div>
@@ -84,6 +90,12 @@ const StoreInfo: React.FC<StoreInfoProps> = ({ isSubmitting }) => {
       >
         {isSubmitting ? <BeatLoader /> : '다음'}
       </button>
+      {isSearchToggle && (
+        <StoreSearchModal
+          onClose={() => setIsSearchToggle(false)}
+          setStoreAddress={setStoreAddress}
+        />
+      )}
     </Form>
   );
 };
