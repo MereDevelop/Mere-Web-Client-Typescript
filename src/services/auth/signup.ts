3219e027f090d5c0347df77956ccf3f29b27eb1d
whitecity01 <kh081104@gmail.com>
2024-01-23 17:38:59 +0900

    fix: api 로직 버전 충돌 해결

--- src/services/auth/signup.ts
+++ src/services/auth/signup.ts
@@ -1,50 +1,30 @@
-import { SERVER_URL } from '@constants/api';
-import { CustomError } from '@custom/types/response';
 import { verifyCodeDto, verifyDto } from '@custom/types/signup/Signup';
-import axios from '@services/config';
+import client from '@services/client';
 import { SignupInfo } from '@store/signup-dto';
 
-export async function requestSignup(signupForm: SignupInfo) {
-  const response = await axios({
-    method: 'post',
-    url: `${SERVER_URL}/owner/sign/up`,
-    data: signupForm,
-  })
-    .then((resData) => {
-      const { status } = resData;
-      return status;
-    })
-    .catch((error): CustomError => error);
+interface ResponseForm {
+  requestId: string;
+  requestTime: string;
+  statusCode: string;
+  statusName: string;
+}
 
+export async function requestSignup(signupForm: SignupInfo) {
+  const response = await client.post<ResponseForm>(`/owner/sign/up`,signupForm)
+  .then((resData) => resData);
+  
   return response;
 }
 
 export async function requestVerifyNumber(verifyDto: verifyDto) {
-  const response = await axios({
-    method: 'post',
-    url: `${SERVER_URL}/owner/sign/in/phone-verification-request`,
-    data: verifyDto,
-  })
-    .then((resData) => {
-      const { status } = resData;
-      return status;
-    })
-    .catch((error): CustomError => error);
+  const response = await client.post<ResponseForm>(`/owner/sign/in/phone-verification-request`,verifyDto)
+  .then((resData) => resData);
 
   return response;
 }
-
 export async function verifyNumber(verifyCodeDto: verifyCodeDto) {
-  const response = await axios({
-    method: 'post',
-    url: `${SERVER_URL}/owner/sign/in/phone-verification`,
-    data: verifyCodeDto,
-  })
-    .then((resData) => {
-      const { status } = resData;
-      return status;
-    })
-    .catch((error): CustomError => error);
-
+  const response = await client.post<ResponseForm>(`/owner/sign/in/phone-verification`,verifyCodeDto)
+  .then((resData) => resData);
+  
   return response;
 }
